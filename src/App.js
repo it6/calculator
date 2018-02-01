df3fcf2f5891abc6702875e5a739fbb7259b2245
Surya Kotamraju <kotamraju@me.com>
2018-01-31 23:10:00 -0500

    build calculator UI/styles, add functionality to display clicked values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    summary: ''
+  };
+
+  handleClick = event => {
+    this.setState({
+      summary: this.state.summary.concat(event.target.innerText)
+    });
+  };
+
   render() {
-    return <div>Calculator</div>;
+    return (
+      <div className="calculator" onClick={this.handleClick}>
+        <div className="row top">{this.state.summary}</div>
+        <div className="row">
+          <CalcButton value="AC" />
+          <CalcButton value="+/_" />
+          <CalcButton value="%" />
+          <CalcButton value="÷" />
+        </div>
+
+        <div className="row">
+          <CalcButton value="7" />
+          <CalcButton value="8" />
+          <CalcButton value="9" />
+          <CalcButton value="x" />
+        </div>
+
+        <div className="row">
+          <CalcButton value="4" />
+          <CalcButton value="5" />
+          <CalcButton value="6" />
+          <CalcButton value="-" />
+        </div>
+
+        <div className="row">
+          <CalcButton value="1" />
+          <CalcButton value="2" />
+          <CalcButton value="3" />
+          <CalcButton value="+" />
+        </div>
+
+        <div className="row">
+          <CalcButton value="0" />
+          <CalcButton value="." />
+          <CalcButton value="=" />
+        </div>
+      </div>
+    );
   }
+}
+
+function CalcButton(props) {
+  return <div className="button">{props.value}</div>;
 }
 
 export default App;
